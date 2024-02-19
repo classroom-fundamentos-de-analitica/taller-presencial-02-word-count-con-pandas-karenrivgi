b1e92a07294cf058a5fc69621ee7f71ddad7ea44
karenrivgi <karenrivgi@gmail.com>
2024-02-19 09:54:52 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -65,7 +65,7 @@ def count_words(dataframe):
 def save_output(dataframe, output_filename):
     """Save output to a file."""
 
-    dataframe.to_csv(output_filename, index=False)
+    dataframe.to_csv(output_filename, index=False , sep='\t', header=False)
 
 
 #
